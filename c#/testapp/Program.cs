0b1401c5c4596e7b94e3eece901b293dbdc5186d
sophes <none@abc.org>
2019-04-22 13:54:10 +0100

    first check in

---
+++ c#/testapp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace testapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = -3;
+            Console.WriteLine("size of an int is {0}", sizeof(int));
+        }
+    }
+}
+    //FIND OUT THE SIZE OF THE FOLLOEING TYPES:
+    - BYTE
+    - SHORT
+    - DECIMAL
+    - BOOL
+    - CHAR
+    - DOUBLE
+    - FLOAT
+    - STRING*
+    //WRITE A PROGRAM THAT PUTS 10 NUMBERS INTO A LIST OR ARRAY, CREATE IT AS ONE AND POPULTAE IT AS THE OTHER *START WITH ARRAY
